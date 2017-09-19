5be2ba51836c2dfc9fa5ba5a5b5b5b95c2cb48db
Maksim Galiev <v-makgal@microsoft.com>
2017-09-19 17:47:15 +0300

    fixed empty commits creation

--- TransferUtils/src/localizationStringsUpdater.ts
+++ TransferUtils/src/localizationStringsUpdater.ts
@@ -1,32 +1,6 @@
 import { DisplayNameAndKeyPairs, IndexedObjects, IndexedFoldersSet, SourceType } from "./models";
 
 export class LocalizationStringsUpdater {
-    public static UpdateDestinationJson(indexedSourceStrings: IndexedObjects, indexedDestinationJson: IndexedObjects): IndexedObjects {
-        let updatedVisuals: IndexedObjects = new IndexedObjects();
-
-        for (let visualName in indexedSourceStrings) {
-            let sourceStrings: DisplayNameAndKeyPairs = indexedSourceStrings[visualName],
-                destinationStrings: DisplayNameAndKeyPairs = indexedDestinationJson[visualName],
-                isUpdated: boolean = false;
-
-            for (let displayNameKey in sourceStrings) {
-                let displayName: string = sourceStrings[displayNameKey];
-
-                if (!destinationStrings[displayNameKey] || destinationStrings[displayNameKey] !== displayName) {
-                    console.log("updated " + visualName + " " + displayName)
-                    destinationStrings[displayNameKey] = displayName;
-                    isUpdated = true;
-                }
-            }
-
-            if (isUpdated) {
-                updatedVisuals[visualName] = destinationStrings;
-            }
-        }
-
-        return updatedVisuals;
-    }
-
     public static UpdateDestinationFolders(indexedSourceStrings: IndexedFoldersSet, indexedDestinationJson: IndexedFoldersSet): IndexedFoldersSet {
         let updatedVisuals: IndexedFoldersSet = new IndexedFoldersSet();
 
@@ -36,14 +10,15 @@ export class LocalizationStringsUpdater {
 
             for (let folderName in folders) {
                 let sourceStrings: DisplayNameAndKeyPairs = folders[folderName],
-                destinationStrings: DisplayNameAndKeyPairs = indexedDestinationJson[visualName][folderName],
+                destinationStrings: DisplayNameAndKeyPairs = new DisplayNameAndKeyPairs(),
                 isUpdated: boolean = false;
 
-                if (!destinationStrings) {                                     
+                if (!indexedDestinationJson[visualName] || !indexedDestinationJson[visualName][folderName]) {                                     
                     destinationStrings = sourceStrings;
                     isUpdated = true;   
                     console.log("added " + visualName + " " + folderName);
                 } else {
+                    destinationStrings = indexedDestinationJson[visualName][folderName];
                     for (let displayNameKey in sourceStrings) {
                         let displayName: string = sourceStrings[displayNameKey];
                         
