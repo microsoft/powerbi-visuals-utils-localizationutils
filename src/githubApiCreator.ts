8a106e38ab9b58e1d4d41d054208942c86581307
aleksandr.savelev <aleksandr.savelev@akvelon.com>
2022-02-17 13:53:51 +0300

    fixed githubapiCreator

--- src/githubApiCreator.ts
+++ src/githubApiCreator.ts
@@ -1,5 +1,4 @@
 import { Octokit } from "@octokit/rest";
-import { config } from "dotenv";
 
 export class GithubApiCreator { 
     private static github: Octokit;    
@@ -7,12 +6,11 @@ export class GithubApiCreator {
 
     public static CreateGithubApi(): Octokit {
         if (!GithubApiCreator.github) {
-            config();
             GithubApiCreator.github = new Octokit({
                 protocol: "https",
                 host: "api.github.com",
                 timeout: 10000,
-                auth: process.env.token,
+                auth: GithubApiCreator.token,
                 headers: {
                     "encoding": "null"
                 },
